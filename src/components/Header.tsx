a65ec70037d64203e5af08bcae149b910bd923a2
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-09 18:46:03 -0300

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <header className="w-full text-center py-4 bg-slate-500">
+            <picture className="text-xl">
+                Gustavo Z.
+            </picture>
+            <nav></nav>
+        </header>
+    )
+}
