7e22eb6fa52b5449138b415137d189b82d8e4cdc
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-10 22:07:02 -0300

    webpage section layout

---
+++ src/layouts/Section.tsx
@@ -0,0 +1,24 @@
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type SectionProps = ComponentProps<'section'> & {
+    title: string,
+    subtitle: string,
+}
+
+export function Section({ children, title, subtitle, className, ...props }: SectionProps) {
+    return (
+        <section className={twMerge('w-full space-y-10', className)} {...props}>
+            <header className="text-center">
+                <h1 className="font-bold text-2xl mb-3">
+                    {title}
+                </h1>
+                <p>
+                    {subtitle}
+                </p>
+            </header>
+
+            {children}
+        </section>
+    )
+}
