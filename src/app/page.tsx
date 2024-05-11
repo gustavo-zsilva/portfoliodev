10067b9b775eb803b6d38f80ca215ac983d8bf29
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-10 21:55:28 -0300

    added projects & gap 10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
+import { Projects } from "@/components/Projects";
 import { Techs } from "@/components/Techs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center px-4 text-white">
-      
+    <main className="flex min-h-screen gap-10 flex-col items-center px-4 text-white">
       <Header />
-
       <Main />
-      
       <Techs />
+      <Projects />
     </main>
   );
 }
