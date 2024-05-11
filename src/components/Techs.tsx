6585b422d75aaba15de6aac2ae1bc2a4d77cd358
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-10 22:06:16 -0300

    refactored

--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -1,27 +1,29 @@
 import StackIcon from 'tech-stack-icons'
 import { promises as fs } from 'fs';
+import { Section } from '@/layouts/Section';
 
 export async function Techs() {
 
     const file = await fs.readFile(process.cwd() + '/stack.json', 'utf8')
     const techList: Object = JSON.parse(file)
 
-    console.log(techList);
-    
-
     return (
-        <section className="w-full text-center">
-            <h1 className="font-bold text-2xl mb-3">Tech Stack</h1>
-            <p>Technologies I most use</p>
-
-            <div className="grid grid-cols-3 gap-6 mt-10">
+        <Section
+            title="Tech Stack"
+            subtitle="Technologies I most use"
+            id="tech-stacks"
+        >
+            <div className="grid grid-cols-3 gap-6">
                 {Object.entries(techList).map(([name, iconCode]) => (
-                    <div className="flex flex-col justify-center items-center gap-2">
+                    <div
+                        key={iconCode}
+                        className="flex flex-col items-center gap-2"
+                    >
                         <StackIcon name={iconCode} className="w-[45px]" />
                         <span className="uppercase text-sm opacity-75">{name}</span>
                     </div>
                 ))}
             </div>
-        </section>
+        </Section>
     )
 }
