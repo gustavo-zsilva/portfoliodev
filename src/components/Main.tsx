9eabb4111d0ab213226656c575b806003d2b5436
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-10 21:56:44 -0300

    made some changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,18 +4,20 @@ import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 
 export function Main() {
     return (
-        <section className="text-2xl font-bold flex flex-col items-center py-6 h-[80vh] justify-center gap-6">
+        <section className="text-2xl flex flex-col items-center justify-center gap-6">
+
             <picture className="bg-red-500 rounded-full p-3 bg-gradient-to-br from-pink to-baby-blue">
-            <Image
-                src="https://github.com/gustavo-zsilva.png"
-                width={100}
-                height={100}
-                alt="Profile Picture"
-                priority
-                className="rounded-full outline outline-8 outline-bg-dark"
-            />
+                <Image
+                    src="https://github.com/gustavo-zsilva.png"
+                    width={100}
+                    height={100}
+                    alt="Profile Picture"
+                    priority
+                    className="rounded-full outline outline-8 outline-dark"
+                />
             </picture>
-            <div className="text-center leading-9 px-4">
+
+            <div className="text-center font-bold leading-9 px-4">
                 <span className="flex justify-center">
                     Hi!&nbsp; <span className="font-normal">👋</span>
                 </span>
@@ -23,11 +25,16 @@ export function Main() {
                 <span className="bg-gradient-to-r from-blue-500 to-baby-blue text-transparent bg-clip-text">
                     Gustavo
                 </span>
-                &nbsp;and I build things for the&nbsp;
+                &nbsp;and I build stuff for the&nbsp;
                 <span className="bg-[url('/underline.png')] bg-contain bg-[center_top_.82rem] bg-no-repeat">
                     web.
                 </span>
             </div>
+
+            <span className="border border-white text-sm px-4 py-1 rounded-full">
+                Florianópolis, Brazil
+            </span>
+
             <p className={`${mono.className} text-sm divide-x-2`}>
                 <span className="px-2">Fullstack Developer</span>
                 <span className="px-2">Freelancer</span>
