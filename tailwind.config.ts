d9cc735793742725a429d3ea11d980ab3052879d
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-10 21:53:58 -0300

    new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,19 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         'white': '#D9D9D9',
-        'bg-dark': '#191919',
+        'dark': '#191919',
+        'light': '#D6DBDC',
         'pink': '#E70FAA',
         'baby-blue': '#00C0FD',
-      }
+        'light-dark': '#232323',
+      },
+      
     },
   },
   plugins: [],
