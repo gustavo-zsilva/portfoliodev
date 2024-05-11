ec4be9eae46a6dc8ce901d85acf2aeeb94d7899b
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-10 21:56:06 -0300

    finally styled header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Socials } from './Socials'
+
 export function Header() {
     return (
-        <header className="w-full text-center py-4 bg-slate-500">
-            <picture className="text-xl">
-                Gustavo Z.
-            </picture>
-            <nav></nav>
+        <header className="w-full h-[20vh] flex flex-col items-center px-4">
+            <Image
+                src="/logo-dark.png"
+                alt="Logo"
+                width={90}
+                height={90}
+            />
+            <nav className="flex flex-wrap gap-x-10 gap-y-2 justify-center">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <a href="#tech-stacks">Tech Stack</a>
+                <a href="#projects">Projects</a>
+                <Link href="/contact">Contact</Link>
+            </nav>
+            <Socials className="mt-4" />
         </header>
     )
 }
