2188d8b1c152836536740dacd4fc100fb439ce9e
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-12 10:48:10 -0300

    created

---
+++ src/components/SkillBox.tsx
@@ -0,0 +1,33 @@
+import { ComponentType } from "react"
+import { IconBaseProps } from 'react-icons'
+
+type SkillBoxProps = {
+    title: string,
+    Icon: ComponentType<IconBaseProps>,
+    iconSize?: number,
+}
+
+export function SkillBox({ title, Icon, iconSize }: SkillBoxProps) {
+    return (
+        <div className="
+            flex
+            flex-col
+            justify-center
+            items-center
+            gap-2
+            size-full
+            p-6
+            border-2
+            border-sky-600
+            border-opacity-25
+            bg-sky-600
+            bg-opacity-10
+            rounded-lg
+        ">
+            <Icon size={iconSize} />
+            <span className="text-sm">
+                {title}
+            </span>
+        </div>
+    )
+}
