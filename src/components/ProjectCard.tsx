0860d030ada695abec2358375cca5c73b15fce0d
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-12 10:49:05 -0300

    improved styles

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import { ExternalLink } from '@/lib/ExternalLink'
 import { MdOutlineStar } from "react-icons/md";
+import { FiArrowUpRight } from "react-icons/fi";
 
 type PinnedRepo = {
     name: string,
@@ -20,19 +21,22 @@ type ProjectCardProps = {
 
 export function ProjectCard({ repo }: ProjectCardProps) {
     return (
-        <div className="bg-gradient-to-br from-pink to-baby-blue rounded-md transition-all hover:p-1">
+        <div className="bg-gradient-to-br from-pink to-baby-blue rounded-xl transition-all hover:p-0.5 hover:rounded-lg">
             <ExternalLink
                 href={repo.url}
-                className="flex flex-col bg-light-dark rounded-md p-4 gap-2"
+                className="flex flex-col bg-dark border-[2px] border-light-dark rounded-lg p-4 gap-2"
             >
-                <h1 className="text-lg uppercase font-bold">
-                    {repo.name}
-                </h1>
-                <p className="line-clamp-2">
+                <header className="flex items-center justify-between">
+                    <h1 className="text-lg font-semibold">
+                        {repo.name}
+                    </h1>
+                    <FiArrowUpRight size={22} className="text-baby-blue" />
+                </header>
+                <p className="line-clamp-2 text-light-gray">
                     {repo.description}
                 </p>
 
-                <footer className="flex items-center justify-between mt-4">
+                <footer className="flex items-center justify-between mt-4 text-light-gray">
                     <div className="flex items-center gap-4">
                         <div className={`
                             bg-[${repo.primaryLanguage.color}]
