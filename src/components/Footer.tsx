86c2c577393a3cfc216d6a1cc6d21f7dddbb26e2
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-11 22:19:49 -0300

    added logo & copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,23 @@
+import Image from 'next/image'
 import { Socials } from "./Socials";
 
 export function Footer() {
     return (
-        <footer className="w-full">
-            <div className="flex items-center gap-4">
+        <footer className="flex flex-col items-center w-full pb-10">
+            <div className="flex items-center gap-4 w-full">
                 <div className="size-0.5 flex flex-1 bg-white rounded-full" />
                 <Socials />
                 <div className="size-0.5 flex flex-1 bg-white rounded-full" />
+            </div>
+
+            <div className="flex flex-col items-center">
+                <Image
+                    src="/logo-dark.png"
+                    alt="logo"
+                    width={90}
+                    height={90}
+                />
+                <span>Made with 💜 by Gustavo Z.</span>
             </div>
         </footer>
     )
