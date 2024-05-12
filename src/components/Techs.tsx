b7e3cbfb23c6613d24dae6edd8b88250dea3e914
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-12 16:38:18 -0300

    adjusted to new stacks.json model

--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -1,29 +1,31 @@
-import StackIcon from 'tech-stack-icons'
-import { promises as fs } from 'fs';
-import { Section } from '@/layouts/Section';
+import StackIcon from "tech-stack-icons";
+import { promises as fs } from "fs";
+import { Section } from "@/layouts/Section";
+
+type Tech = {
+  name: string;
+  iconName: string;
+  experience: number;
+};
 
 export async function Techs() {
+  const file = await fs.readFile(process.cwd() + "/stack.json", "utf8");
+  const techList: Tech[] = JSON.parse(file);
 
-    const file = await fs.readFile(process.cwd() + '/stack.json', 'utf8')
-    const techList: Object = JSON.parse(file)
-
-    return (
-        <Section
-            title="Tech Stack"
-            subtitle="Technologies I most use"
-            id="tech-stacks"
-        >
-            <div className="grid grid-cols-3 gap-6">
-                {Object.entries(techList).map(([name, iconCode]) => (
-                    <div
-                        key={iconCode}
-                        className="flex flex-col items-center gap-2"
-                    >
-                        <StackIcon name={iconCode} className="w-[45px]" />
-                        <span className="uppercase text-sm text-light-gray">{name}</span>
-                    </div>
-                ))}
-            </div>
-        </Section>
-    )
+  return (
+    <Section
+      title="Tech Stack"
+      subtitle="Technologies I most use"
+      id="tech-stacks"
+    >
+      <div className="grid grid-cols-3 gap-6">
+        {techList.map(({ name, iconName }) => (
+          <div key={iconName} className="flex flex-col items-center gap-2">
+            <StackIcon name={iconName} className="w-[45px]" />
+            <span className="uppercase text-sm text-light-gray">{name}</span>
+          </div>
+        ))}
+      </div>
+    </Section>
+  );
 }
