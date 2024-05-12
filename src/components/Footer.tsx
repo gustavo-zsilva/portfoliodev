c557f5980669b1a18f52beced1b9e9cf20e7bffd
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-11 22:25:51 -0300

    finished mobile footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,16 @@ import { Socials } from "./Socials";
 
 export function Footer() {
     return (
-        <footer className="flex flex-col items-center w-full pb-10">
+        <footer className="flex flex-col items-center w-full gap-6 pb-10">
             <div className="flex items-center gap-4 w-full">
                 <div className="size-0.5 flex flex-1 bg-white rounded-full" />
                 <Socials />
                 <div className="size-0.5 flex flex-1 bg-white rounded-full" />
+            </div>
+
+            <div className="text-center">
+                <p>+55 48 98847-9786</p>
+                <p>gustavozontadasilva@gmail.com</p>
             </div>
 
             <div className="flex flex-col items-center">
