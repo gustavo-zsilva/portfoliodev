8db383877e0a79b5733d2d25b0e2969f04c023b5
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-11 22:40:24 -0300

    client components & active links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { Socials } from './Socials'
 
 export function Header() {
+    const pathname = usePathname()
+
     return (
-        <header className="w-full h-[20vh] flex flex-col items-center px-4">
+        <header className="w-full flex flex-col items-center px-4">
             <Image
                 src="/logo-dark.png"
                 alt="Logo"
@@ -13,11 +18,11 @@ export function Header() {
                 height={90}
             />
             <nav className="flex flex-wrap gap-x-10 gap-y-2 justify-center">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
+                <Link href="/" className={`${pathname === '/' && 'font-semibold'}`}>Home</Link>
+                <Link href="/about" className={`${pathname === '/about' && 'font-semibold'}`}>About</Link>
                 <a href="#tech-stacks">Tech Stack</a>
                 <a href="#projects">Projects</a>
-                <Link href="/contact">Contact</Link>
+                <Link href="/contact" className={`${pathname === '/contact' && 'font-semibold'}`}>Contact</Link>
             </nav>
             <Socials className="mt-4" />
         </header>
