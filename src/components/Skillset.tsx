cd171f4e91e25edea797c65c4fd85cac0514f8a6
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-13 14:14:44 -0300

    finished infinite scroll

--- src/components/Skillset.tsx
+++ src/components/Skillset.tsx
@@ -20,33 +20,81 @@ export async function Skillset() {
   };
 
   return (
-    <Section title="Skillset" className="w-screen px-6">
-      <ul className="flex w-full overflow-x-scroll gap-4">
-        {techList.map(({ name, iconName, experience }) => (
-          <li key={iconName} className="flex flex-col gap-3 min-w-[10rem]">
-            <div className="flex items-center gap-3">
-              <StackIcon name={iconName} className="w-[40px]" />
-              <h1 className="font-semibold">{name}</h1>
-            </div>
+    <Section
+      title="Skillset"
+      className="
+        w-screen
+        max-w-[750px]
+        m-auto
+      "
+    >
+      <div
+        className="
+          inline-flex
+          flex-nowrap
+          gap-4
+          w-full
+          px-6
+          overflow-hidden
+          [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]
+        "
+      >
+        <ul className="flex items-center justify-center md:justify-start gap-4 animate-infinite-scroll">
+          {techList.map(({ name, iconName, experience }) => (
+            <li key={iconName} className="flex flex-col gap-3 min-w-[10rem]">
+              <div className="flex items-center gap-3">
+                <StackIcon name={iconName} className="w-[40px]" />
+                <h1 className="font-semibold">{name}</h1>
+              </div>
 
-            <div className="flex ring-1 ring-inset ring-light-gray w-full h-4 relative rounded-full overflow-hidden">
-              <div
-                className={`
-                    absolute
-                    rounded-full
-                    bg-gradient-to-r
-                    from-blue-500
-                    to-baby-blue
-                    top-0
-                    bottom-0
-                    left-0
-                    ${progressBarWidth[experience as keyof typeof progressBarWidth]}
-                  `}
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
+              <div className="flex ring-1 ring-inset ring-light-gray w-full h-4 relative rounded-full overflow-hidden">
+                <div
+                  className={`
+                      absolute
+                      rounded-full
+                      bg-gradient-to-r
+                      from-blue-500
+                      to-baby-blue
+                      top-0
+                      bottom-0
+                      left-0
+                      ${progressBarWidth[experience as keyof typeof progressBarWidth]}
+                    `}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start gap-4 animate-infinite-scroll"
+          aria-hidden="true"
+        >
+          {techList.map(({ name, iconName, experience }) => (
+            <li key={iconName} className="flex flex-col gap-3 min-w-[10rem]">
+              <div className="flex items-center gap-3">
+                <StackIcon name={iconName} className="w-[40px]" />
+                <h1 className="font-semibold">{name}</h1>
+              </div>
+
+              <div className="flex ring-1 ring-inset ring-light-gray w-full h-4 relative rounded-full overflow-hidden">
+                <div
+                  className={`
+                      absolute
+                      rounded-full
+                      bg-gradient-to-r
+                      from-blue-500
+                      to-baby-blue
+                      top-0
+                      bottom-0
+                      left-0
+                      ${progressBarWidth[experience as keyof typeof progressBarWidth]}
+                    `}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Section>
   );
 }
