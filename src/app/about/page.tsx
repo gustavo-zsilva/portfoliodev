c3e905a58a9758bea09854d3cc151039e21bfeff
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-13 17:33:22 -0300

    finished contact page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import { ExperienceSection } from "@/components/ExperienceSection";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Skillset } from "@/components/Skillset";
 import { Section } from "@/layouts/Section";
+
+import { FaArrowRightLong } from "react-icons/fa6";
+
+import Link from "next/link";
 
 export default async function About() {
   return (
@@ -16,13 +21,47 @@ export default async function About() {
         </p>
       </Section>
       <Skillset />
-      <Section title="Work Experience" className="px-6">
-        <div className="flex">
-          <div className="h-[400px] w-1 bg-white rounded-full" />
-          <section className="flex">
-            <div className="size-4 bg-white rounded-full ring-4 ring-white ring-offset-4 ring-offset-dark -translate-x-[60%]" />
-            <p>work experience</p>
-          </section>
+      <Section title="My Experience" className="px-6">
+        <div className="border-l-4 border-dotted flex flex-col gap-10">
+          <ExperienceSection title="Started Journey" year="2019">
+            Started my journey on the dev life, learning C# & Python as my first
+            languages
+          </ExperienceSection>
+          <ExperienceSection title="Discovered Javascript" year="2020">
+            Started building basic apps for the web
+          </ExperienceSection>
+          <ExperienceSection title="Advancing my Studies" year="2020 - 2023">
+            Mastered React, NextJS, Typescript & web technologies
+          </ExperienceSection>
+          <ExperienceSection
+            title="Building for Real People"
+            year="2024 - Today"
+          >
+            Building high standard, performatic web apps for companies &
+            microentrepreneurs
+            <Link
+              href="/contact"
+              className="
+                flex
+                items-center
+                gap-4
+                bg-blue-500
+                py-2
+                px-6
+                mt-6
+                w-fit
+                font-semibold
+                transition-shadow
+                shadow-lg
+                rounded-lg
+                hover:bg-blue-600
+                hover:shadow-blue-500/35
+              "
+            >
+              Contact Me
+              <FaArrowRightLong size={20} />
+            </Link>
+          </ExperienceSection>
         </div>
       </Section>
       <Footer />
