d58195da6df5cd8362c8f331f5ccf65fc5d8505f
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-13 17:33:56 -0300

    working on fixing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,42 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
-import { Socials } from './Socials'
+import { Socials } from "./Socials";
 
 export function Header() {
-    const pathname = usePathname()
+  const pathname = usePathname();
+  const router = useRouter();
 
-    return (
-        <header className="w-full flex flex-col items-center px-4">
-            <Image
-                src="/logo-dark.png"
-                alt="Logo"
-                width={90}
-                height={90}
-            />
-            <nav className="flex flex-wrap gap-x-10 gap-y-2 justify-center">
-                <Link href="/" className={`${pathname === '/' && 'font-semibold'}`}>Home</Link>
-                <Link href="/about" className={`${pathname === '/about' && 'font-semibold'}`}>About</Link>
-                <a href="#tech-stacks">Tech Stack</a>
-                <a href="#projects">Projects</a>
-                <Link href="/contact" className={`${pathname === '/contact' && 'font-semibold'}`}>Contact</Link>
-            </nav>
-            <Socials className="mt-4" />
-        </header>
-    )
+  function redirectToHome() {
+    router.push("/");
+  }
+
+  return (
+    <header className="w-full flex flex-col items-center px-4">
+      <Image src="/logo-dark.png" alt="Logo" width={90} height={90} />
+      <nav className="flex flex-wrap gap-x-10 gap-y-2 justify-center">
+        <Link href="/" className={`${pathname === "/" && "font-semibold"}`}>
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={`${pathname === "/about" && "font-semibold"}`}
+        >
+          About
+        </Link>
+        <button onClick={redirectToHome}>Tech Stack</button>
+        <button>Projects</button>
+        <Link
+          href="/contact"
+          className={`${pathname === "/contact" && "font-semibold"}`}
+        >
+          Contact
+        </Link>
+      </nav>
+      <Socials className="mt-4" />
+    </header>
+  );
 }
