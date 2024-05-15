5996ded12a5a863c1392f5b1020de71bccfa06a6
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-15 10:23:57 -0300

    gh secret in env

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,34 +1,36 @@
 import { Section } from "@/layouts/Section";
-import { Client } from 'get-pinned-repos';
+import { Client } from "get-pinned-repos";
 import { ProjectCard } from "./ProjectCard";
 
 type PinnedRepo = {
-    name: string,
-    url: string,
-    primaryLanguage: {
-        name: string,
-        color: string,
-        id: string,
-    },
-    id: string,
-    description: string,
-    stargazerCount: number,
-}
-
+  name: string;
+  url: string;
+  primaryLanguage: {
+    name: string;
+    color: string;
+    id: string;
+  };
+  id: string;
+  description: string;
+  stargazerCount: number;
+};
 
 export async function Projects() {
-    Client.setToken("MY_GITHUB_SECRET")
-    const pinnedRepos: PinnedRepo[] = await Client.getPinnedRepos("gustavo-zsilva")
+  Client.setToken(process.env.GITHUB_TOKEN);
+  const pinnedRepos: PinnedRepo[] =
+    await Client.getPinnedRepos("gustavo-zsilva");
 
-    return (
-        <Section
-            title="Projects"
-            subtitle="Some of my best creations"
-            id="projects"
-        >
-            <ul className="space-y-6">
-                {pinnedRepos.map(repo => <ProjectCard key={repo.id} repo={repo} />)}
-            </ul>
-        </Section>
-    )
+  return (
+    <Section
+      title="Projects"
+      subtitle="Some of my best creations"
+      id="projects"
+    >
+      <ul className="space-y-6">
+        {pinnedRepos.map((repo) => (
+          <ProjectCard key={repo.id} repo={repo} />
+        ))}
+      </ul>
+    </Section>
+  );
 }
