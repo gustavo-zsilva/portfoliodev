474eb2872e2de962326cff4492fcd31bf3dedb58
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-17 16:01:11 -0300

    created scoped client component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+type NavigationProps = ComponentProps<"nav">;
+
+export function Navigation({ className }: NavigationProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  function redirectToSection(elementId: string) {
+    router.push(`/${elementId}`);
+  }
+
+  return (
+    <nav
+      className={twMerge(
+        `flex flex-wrap gap-x-10 gap-y-2 justify-center`,
+        className,
+      )}
+    >
+      <Link href="/" className={`${pathname === "/" && "font-semibold"}`}>
+        Home
+      </Link>
+      <Link
+        href="/about"
+        className={`${pathname === "/about" && "font-semibold"}`}
+      >
+        About
+      </Link>
+      <button onClick={() => redirectToSection("#tech-stacks")}>
+        Tech Stack
+      </button>
+      <button onClick={() => redirectToSection("#how-i-work")}>
+        How I Work
+      </button>
+      <button onClick={() => redirectToSection("#projects")}>Projects</button>
+      <Link
+        href="/contact"
+        className={`${pathname === "/contact" && "font-semibold"}`}
+      >
+        Contact
+      </Link>
+    </nav>
+  );
+}
