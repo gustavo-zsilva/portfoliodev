af870419a9d23a3fabe645764c535596c5890960
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-17 16:14:21 -0300

    link logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { Socials } from "./Socials";
 import { Navigation } from "./Navigation";
@@ -6,13 +7,15 @@ import { Navigation } from "./Navigation";
 export function Header() {
   return (
     <header className="w-full flex flex-col lg:flex-row items-center px-4 lg:justify-between">
-      <Image
-        src="/images/logo-dark.png"
-        alt="Logo"
-        width={90}
-        height={90}
-        priority
-      />
+      <Link href="/">
+        <Image
+          src="/images/logo-dark.png"
+          alt="Logo"
+          width={90}
+          height={90}
+          priority
+        />
+      </Link>
       <Navigation />
       <Socials className="mt-4 lg:m-0" />
     </header>
