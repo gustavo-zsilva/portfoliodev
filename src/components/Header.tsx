2b5846d2488af93b82989460c9835c24b14b7885
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-17 19:53:42 -0300

    added theme button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,27 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import { Socials } from "./Socials";
 import { Navigation } from "./Navigation";
+import { ThemeSwitch } from "./ThemeSwitch";
+import { ThemeImage } from "./ThemeImage";
 
 export function Header() {
   return (
     <header className="w-full flex flex-col lg:flex-row items-center px-4 lg:justify-between">
       <Link href="/">
-        <Image
-          src="/images/logo-dark.png"
+        <ThemeImage
+          srcLight="/images/logo-light.png"
+          srcDark="/images/logo-dark.png"
           alt="Logo"
           width={90}
           height={90}
-          priority
         />
       </Link>
       <Navigation />
-      <Socials className="mt-4 lg:m-0" />
+      <div className="flex items-center mt-4 lg:m-0 gap-4">
+        <Socials />
+        <ThemeSwitch />
+      </div>
     </header>
   );
 }
